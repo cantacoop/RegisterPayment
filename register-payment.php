dc7416f44f58a47c40084e3a0a9f719fcb27f847
Canta Coop <cantacoop@gmail.com>
2018-02-21 14:05:36 +0700

    Initialize Project

---
+++ register-payment.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:  Register Payment
+Plugin URI:   https://github.com/cantacoop/RegisterPayment
+Description:  Basic WordPress Plugin Register Payment
+Version:      1.0.0
+Author:       Cantacoop
+Author URI:   https://www.linkedin.com/in/cantacoop/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  register-payment
+*/
